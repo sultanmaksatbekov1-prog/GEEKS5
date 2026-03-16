9c0a4ab70fb2a3c4ca42ddc63f93ee0ffefaff48
_Borz <sultanmaksatbekov1@gmail.com>
2026-03-16 17:31:02 +0600

    hhg

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import ChildButton from "./components/ChildButton";
-import Counter from "./components/Counter";
-import FilteredList from "./components/FilteredList";
-import SearchInput from "./components/SearchInput";
-import UsersList from "./modules/UsersList";
 
 function App() {
-  return (
-    <div>
-      {/* <h2>React</h2> */}
-      {/* <Block /> */}
-      {/* <UsersList /> */}
-      {/* <SearchInput /> */}
-      <ChildButton />
-      <Counter />
-      <FilteredList />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
